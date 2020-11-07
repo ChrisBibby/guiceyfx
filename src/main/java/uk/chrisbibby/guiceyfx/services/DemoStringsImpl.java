1659143026ba225d0525a00b8413c1c62cc97bc4
Chris Bibby <github@niroe.co.uk>
2020-11-07 00:36:47 +0000

    (chore): fixed typo

--- src/main/java/uk/chrisbibby/guiceyfx/services/DemoStringsImpl.java
+++ src/main/java/uk/chrisbibby/guiceyfx/services/DemoStringsImpl.java
@@ -3,7 +3,7 @@ package uk.chrisbibby.guiceyfx.services;
 public class DemoStringsImpl implements DemoStrings {
 
   private final String title = "GuiceyFX - JavaFX & Guice Demo";
-  private final String header = "JavaFX 14 & Guice 15";
+  private final String header = "JavaFX 14 & Guice 5";
   private final String content = "Demo to illustrate wiring JavaFX and Guice";
 
   @Override
